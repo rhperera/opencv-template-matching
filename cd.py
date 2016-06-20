37731d54cc13a19e672251724ae7195cc51ecc98
rhperera <harith9127@gmail.com>
2016-06-20 23:11:33 +0530

    laplacian derivative filter added

--- cd.py
+++ cd.py
@@ -14,12 +14,14 @@ t_diamonds  = {"l":"d","t":cv2.imread('CardsPng/diamonds.jpg',0)}
 t_diamonds2  = {"l":"d","t":cv2.imread('CardsPng/diamonds2.jpg',0)}
 
 test1  = cv2.imread(fileName)
-test1gray = cv2.cvtColor(test1, cv2.COLOR_BGR2GRAY)
+test1gray = cv2.Laplacian(test1,cv2.CV_8U)
+test1gray = cv2.cvtColor(test1gray, cv2.COLOR_BGR2GRAY)
 templateArray = [t_hearts,t_hearts2,t_spades,t_spades2,
                 t_clubs,t_clubs2,t_diamonds,t_diamonds2]
 
 for t in templateArray:
-    results = cv2.matchTemplate(test1gray,t['t'], cv2.TM_CCOEFF_NORMED)
+    temp = cv2.Laplacian(t['t'],cv2.CV_8U)
+    results = cv2.matchTemplate(test1gray,temp, cv2.TM_CCOEFF_NORMED)
     locations = np.where(results >= 0.95)
     if len(locations[0])>=1:
         w, h = t['t'].shape[::-1]
@@ -29,12 +31,6 @@ for t in templateArray:
         #print(cardClass)
         break
 
-#width, height = test1gray.shape[::-1]
-#r = 1000.0 / test1gray.shape[1]
-#dim = (1000, int(test1gray.shape[0] * r))
-#test1gray = cv2.resize(test1gray,dim, interpolation = cv2.INTER_AREA)
-#print(test1gray.shape)
-#print(locations)
 if cardClass=="h":
     print("Hearts")
 elif cardClass=="s":
@@ -43,7 +39,8 @@ elif cardClass=="c":
     print("Clubs")
 elif cardClass=="d":
     print("Diamonds")
-
+else:
+    print("Unable to detect")
 
 cv2.imshow('detected',test1gray)
 cv2.waitKey(0)
